6c8ce13bb6b63d6294993bc042e35be3813fb4cf
mohitch011 <chmohit00@gmail.com>
2025-03-19 13:31:04 +0530

    as

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -5,8 +5,9 @@ import { Route, Routes } from 'react-router-dom'
 import Add from './pages/Add/Add'
 import List from './pages/List/List'
 import Orders from './pages/Orders/Orders'
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import './App.css' // Importing the CSS file
 
 const App = () => {
   return (
@@ -15,6 +16,12 @@ const App = () => {
       <Navbar />
       <hr />
       <div className="app-content">
+        <div className="parent">
+          <div className="div4">4</div>
+          <div className="div5">5</div>
+          <div className="div6">6</div>
+        </div>
+
         <Sidebar />
         <Routes>
           <Route path="/add" element={<Add />} />
