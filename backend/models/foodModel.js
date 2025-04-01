20bc6491e49af24dee39e60e7c62112fbd4db518
MohitCh011 <146988404+MohitCh011@users.noreply.github.com>
2025-04-01 15:42:11 +0530

    Update foodModel.js

--- backend/models/foodModel.js
+++ backend/models/foodModel.js
@@ -3,10 +3,11 @@ import mongoose from "mongoose";
 const foodSchema = new mongoose.Schema({
     name: { type: String, required: true },
     description: { type: String, required: true },
-    price: { type: Number, required: true},
-    image: { type: String },
-    category:{ type:String, required:true}
-})
+    price: { type: Number, required: true },
+    imageUrl: { type: String, required: true },  // Cloudinary image URL
+    imagePublicId: { type: String, required: true },  // Cloudinary public ID for deletion
+    category: { type: String, required: true }
+});
 
 const foodModel = mongoose.models.food || mongoose.model("food", foodSchema);
 export default foodModel;
