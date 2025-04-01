1cfcb23b3a5f8a88f4acd21ab7817aef5af7cf82
MohitCh011 <146988404+MohitCh011@users.noreply.github.com>
2025-04-01 15:46:16 +0530

    Update Cart.jsx

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,15 +1,14 @@
-import React, { useContext } from 'react'
-import './Cart.css'
-import { StoreContext } from '../../Context/StoreContext'
+import React, { useContext } from 'react';
+import './Cart.css';
+import { StoreContext } from '../../Context/StoreContext';
 import { useNavigate } from 'react-router-dom';
 
 const Cart = () => {
-
-  const {cartItems, food_list, removeFromCart,getTotalCartAmount,url,currency,deliveryCharge} = useContext(StoreContext);
+  const { cartItems, food_list, removeFromCart, getTotalCartAmount, url, currency, deliveryCharge } = useContext(StoreContext);
   const navigate = useNavigate();
 
   return (
-    <div className='cart'>
+    <div className="cart">
       <div className="cart-items">
         <div className="cart-items-title">
           <p>Items</p> <p>Title</p> <p>Price</p> <p>Quantity</p> <p>Total</p> <p>Remove</p>
@@ -17,18 +16,21 @@ const Cart = () => {
         <br />
         <hr />
         {food_list.map((item, index) => {
-          if (cartItems[item._id]>0) {
-            return (<div key={index}>
-              <div className="cart-items-title cart-items-item">
-                <img src={url+"/images/"+item.image} alt="" />
-                <p>{item.name}</p>
-                <p>{currency}{item.price}</p>
-                <div>{cartItems[item._id]}</div>
-                <p>{currency}{item.price*cartItems[item._id]}</p>
-                <p className='cart-items-remove-icon' onClick={()=>removeFromCart(item._id)}>x</p>
+          if (cartItems[item._id] > 0) {
+            return (
+              <div key={index}>
+                <div className="cart-items-title cart-items-item">
+                  {/* Make sure the image uses full Cloudinary URL */}
+                  <img src={item.imageUrl} alt={item.name} />
+                  <p>{item.name}</p>
+                  <p>{currency}{item.price}</p>
+                  <div>{cartItems[item._id]}</div>
+                  <p>{currency}{item.price * cartItems[item._id]}</p>
+                  <p className="cart-items-remove-icon" onClick={() => removeFromCart(item._id)}>x</p>
+                </div>
+                <hr />
               </div>
-              <hr />
-            </div>)
+            );
           }
         })}
       </div>
@@ -36,26 +38,32 @@ const Cart = () => {
         <div className="cart-total">
           <h2>Cart Totals</h2>
           <div>
-            <div className="cart-total-details"><p>Subtotal</p><p>{currency}{getTotalCartAmount()}</p></div>
+            <div className="cart-total-details">
+              <p>Subtotal</p><p>{currency}{getTotalCartAmount()}</p>
+            </div>
             <hr />
-            <div className="cart-total-details"><p>Delivery Fee</p><p>{currency}{getTotalCartAmount()===0?0:deliveryCharge}</p></div>
+            <div className="cart-total-details">
+              <p>Delivery Fee</p><p>{currency}{getTotalCartAmount() === 0 ? 0 : deliveryCharge}</p>
+            </div>
             <hr />
-            <div className="cart-total-details"><b>Total</b><b>{currency}{getTotalCartAmount()===0?0:getTotalCartAmount()+deliveryCharge}</b></div>
+            <div className="cart-total-details">
+              <b>Total</b><b>{currency}{getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + deliveryCharge}</b>
+            </div>
           </div>
-          <button onClick={()=>navigate('/order')}>PROCEED TO CHECKOUT</button>
+          <button onClick={() => navigate('/order')}>PROCEED TO CHECKOUT</button>
         </div>
         <div className="cart-promocode">
           <div>
             <p>If you have a promo code, Enter it here</p>
-            <div className='cart-promocode-input'>
-              <input type="text" placeholder='promo code'/>
+            <div className="cart-promocode-input">
+              <input type="text" placeholder="promo code" />
               <button>Submit</button>
             </div>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Cart
+export default Cart;
